786549081c709bf9b1fe6cf9a9c3165b29c372bb
SD from new MJ HP Laptop <saptadeep.dutta@gmail.com>
2023-05-03 09:15:05 +0530

    Changes to be committed: 	modified:   python.bat

--- python.bat
+++ python.bat
@@ -7,7 +7,6 @@
 @echo.
 @echo.
 @echo.
-@echo.
 
 set PATH=C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Library\mingw-w64\bin;C:\ProgramData\Anaconda3\Library\usr\bin;C:\ProgramData\Anaconda3\Library\bin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\bin;C:\ProgramData\Anaconda3\condabin;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0;C:\windows\System32\OpenSSH;C:\Users\drmee\AppData\Local\Microsoft\WindowsApps;%PATH%;
 python.exe
