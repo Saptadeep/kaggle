339a98a1df5234298bb1bfc618e7b409ca5c24c0
SD from new MJ HP Laptop <saptadeep.dutta@gmail.com>
2023-05-03 09:06:56 +0530

    First commit

---
+++ python.bat
@@ -0,0 +1,14 @@
+@cls
+@echo off
+@echo.
+@echo.
+@echo.
+@echo                          Running python.bat in the folder C:\kaggle
+@echo.
+@echo.
+@echo.
+@echo.
+
+set PATH=C:\ProgramData\Anaconda3;C:\ProgramData\Anaconda3\Library\mingw-w64\bin;C:\ProgramData\Anaconda3\Library\usr\bin;C:\ProgramData\Anaconda3\Library\bin;C:\ProgramData\Anaconda3\Scripts;C:\ProgramData\Anaconda3\bin;C:\ProgramData\Anaconda3\condabin;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0;C:\windows\System32\OpenSSH;C:\Users\drmee\AppData\Local\Microsoft\WindowsApps;%PATH%;
+python.exe
+
